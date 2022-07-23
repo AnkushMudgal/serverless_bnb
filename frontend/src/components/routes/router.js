4fb30450291ade78f0170415fec902ff87a24406
Arjun Naravula Loganathan <ar302948@dal.ca>
2022-07-23 17:00:17 -0300

    Adding Analytics Module

--- frontend/src/components/routes/router.js
+++ frontend/src/components/routes/router.js
@@ -1,18 +1,22 @@
-import {BrowserRouter, Switch} from "react-router-dom";
-import {ProtectedRoute} from "./ProtectedRoute";
-import {routes} from "../../constants";
-import {HomePage} from "../HomePage";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ProtectedRoute } from "./ProtectedRoute";
+import { routes } from "../../constants";
+import { HomePage } from "../HomePage";
+import { Analytics } from "../Analytics";
 
 function Routing() {
     return (
         <BrowserRouter>
             <Switch>
                 <ProtectedRoute exact path={routes.home}>
-                    <HomePage/>
+                    <HomePage />
                 </ProtectedRoute>
             </Switch>
+            <Route exact path="/analysis">
+                <Analytics />
+            </Route>
         </BrowserRouter>
     )
 }
 
-export {Routing}
+export { Routing }
