b1b129d42e9a3b76bd4d5399c348e774a86312b7
unknown <dm341240@dal.ca>
2022-07-23 15:43:26 -0300

    authentication forms

---
+++ frontend/src/components/ceaserCipher.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import { FormLabel } from "@mui/material"
+import axios from "axios"
+
+export default function CeaserCipherAuth() {
+    const paperStyle = {
+        padding: 20,
+        height: "70vh",
+        width: 400,
+        margin: "20px auto",
+    }
+
+    const [encryptedCipher, setEncryptedCipher] = React.useState("")
+    const btnstyle = { margin: "8px 0" }
+    const [disableButton, setDisableButton] = React.useState(true)
+    const [errorMessage, setErrorMessage] = React.useState("")
+    const [decryptedCipher, setDecryptedCipher] = React.useState("")
+    const [validDecryptedCipherText, setValidDecryptedCipherText] =
+        React.useState("")
+
+    React.useEffect(() => {
+        axios
+            .get(
+                "https://us-central1-b00904831-a4-partb.cloudfunctions.net/ceaser_cipher/generateEncryptedCode"
+            )
+            .then((res) => {
+                setEncryptedCipher(res.data)
+            })
+    }, [])
+
+    const handleOnChange = (event) => {
+        if (event.target.value && event.target.value !== "") {
+            setDecryptedCipher(event.target.value)
+            setDisableButton(false)
+        } else setDisableButton(true)
+    }
+
+    //TODO: cloudFunction
+    const handleSubmit = async (e) => {
+        await axios
+            .post(
+                "https://us-central1-b00904831-a4-partb.cloudfunctions.net/ceaser_cipher/decryptCipher",
+                {
+                    encryptedCode: encryptedCipher,
+                    key: location.state,
+                }
+            )
+            .then((res) => {
+                if (res.data && decryptedCipher === res.data) {
+                    navigate("/")
+                } else setErrorMessage("Incorrect Decrypted Cipher Text Entered")
+            })
+    }
+
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                    <h2 sx={{ fontWeight: "bold" }}>BB - Login Page 3-3</h2>
+                </Grid>
+                <br />
+                <FormLabel id="encryptedCipher" sx={{ fontWeight: "bold" }}>
+                    Decrypt Ceaser Cipher : {encryptedCipher}
+                </FormLabel>
+                <br />
+                <br />
+                <TextField
+                    label="Enter Decrypted Cipher Text"
+                    placeholder="Enter Decrypted Cipher Text"
+                    fullWidth
+                    required
+                    id="decryptedCipherText"
+                    onChange={(event) => {
+                        handleOnChange(event)
+                    }}
+                //   value={userDetails.email}
+                //   error={errors['decryptedCipherText'] ? true : false}
+                //   helperText={errors['decryptedCipherText'] ? '.' : ''}
+                />
+                <br />
+                <br />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    style={btnstyle}
+                    fullWidth
+                    disabled={disableButton}
+                    onClick={handleSubmit}
+                >
+                    Login
+                </Button>
+                {errorMessage != null && <p>{errorMessage}</p>}
+            </Paper>
+        </Grid>
+    )
+}
---
+++ frontend/src/components/questionAndAnswer.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react"
+import Paper from "@mui/material/Paper"
+import Grid from "@mui/material/Grid"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+import axios from "axios"
+import { Typography } from "@mui/material"
+import { useHistory } from "react-router-dom"
+import { routes } from "../constants"
+
+export default function QuestionAndAnswer() {
+    const paperStyle = {
+        padding: 20,
+        height: "70vh",
+        width: 400,
+        margin: "20px auto",
+    }
+    const history = useHistory();
+    const btnstyle = { margin: "8px 0" }
+    const [disableLogin, setDisableLogin] = React.useState(true)
+    const [errorMessage, setErrorMessage] = React.useState("")
+    const [emailID, setEmailID] = React.useState("")
+    const [securityAnswer1, setSecurityAnswer1] = React.useState("")
+    const [securityAnswer2, setSecurityAnswer2] = React.useState("")
+    const [securityAnswer3, setSecurityAnswer3] = React.useState("")
+    //let navigate = useNavigate()
+    //const location = useLocation()
+
+    const handleSubmit = async (e) => {
+        const securityDetails = {
+            email_id: localStorage.getItem("CurrentUser"),
+            security_details: [
+                {
+                    securityAnswer: securityAnswer1,
+                    securityQuestion: "q1",
+                },
+                {
+                    securityAnswer: securityAnswer2,
+                    securityQuestion: "q2",
+                },
+                {
+                    securityAnswer: securityAnswer3,
+                    securityQuestion: "q3",
+                },
+            ],
+        }
+
+        console.log("security details", securityDetails)
+        axios
+            .post(
+                "https://qki325g7l5cutyw24ykbobkvsq0fxghf.lambda-url.us-east-1.on.aws/",
+                securityDetails
+            )
+            .then((res) => {
+                if (res.data.valid)
+                    history.push(routes.ceaserCipher);
+                else setErrorMessage("Authentication Failed - Invalid Security answers")
+            })
+    }
+
+    return (
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align="center">
+                    <h2>BB - Login Page 2-3</h2>
+                    <br />
+                </Grid>
+                <Typography variant="subtitle2" gutterBottom component="div">
+                    What city were you born in?
+                </Typography>
+                <TextField
+                    label="ans1"
+                    placeholder="Enter answer"
+                    fullWidth
+                    required
+                    id="ans1"
+                    value={securityAnswer1}
+                    onChange={(event) => {
+                        setSecurityAnswer1(event.target.value)
+                    }}
+                />
+                <br />
+                <br />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                    What is your mother's maiden name?
+                </Typography>
+                <TextField
+                    label="ans2"
+                    placeholder="Enter answer"
+                    fullWidth
+                    required
+                    id="ans2"
+                    value={securityAnswer2}
+                    onChange={(event) => {
+                        setSecurityAnswer2(event.target.value)
+                    }}
+                />
+                <br />
+                <br />
+                <Typography variant="subtitle2" gutterBottom component="div">
+                    What is your favourite colour
+                </Typography>
+                <TextField
+                    label="ans3"
+                    placeholder="Enter answer"
+                    fullWidth
+                    required
+                    id="ans3"
+                    value={securityAnswer3}
+                    onChange={(event) => {
+                        setSecurityAnswer3(event.target.value)
+                    }}
+                />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    style={btnstyle}
+                    fullWidth
+                    //disabled={disableLogin}
+                    onClick={handleSubmit}
+                    sx={{ backgroundColor: "#4a4a4a" }}
+                >
+                    Verify
+                </Button>
+                {errorMessage != null && <p>{errorMessage}</p>}
+            </Paper>
+        </Grid>
+    )
+}
