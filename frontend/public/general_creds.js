4d271544c866bc1fa9c41b137d4a52ed50b3f7b5
Ankush Mudgal <an564394@dal.ca>
2022-07-24 18:40:43 -0300

    Online Support Bot - Code for General And Registered User Bots.

---
+++ frontend/public/general_creds.js
@@ -0,0 +1,5 @@
+aws_access_key_id = 'ASIA3MOKDWMXRAGKPBW5'
+aws_secret_access_key = 'ZCzA4rh0HoQGRvsFXNhJoZQSorkyoiscRE7ax7aX'
+aws_session_token = 'FwoGZXIvYXdzEF8aDIH3cZye/0opE/P+siLAARTxSyHBlcvSIupUaNWkuP/oFjAUXJwTnU5CSL+U5LaLgW473fetJQPXRvsjNLf6Vk+zKey0n4mafFCnwmcbla2XWlLd6zUw4b0SfFE8o2mHfYKIYwD6bInvz+IUh4scaD9rkdV0i8sSx2MKq4U/Gfd74R62hsmdygtOctSdw9CSgOQVdYcDM3wDJMsGXQQwszWWNs9CCEwhJ0cbgM2mgQzjs3iTq/2fUBn913LVvmS+urhY3zBFl84DNwaC68aAfijk6/aWBjItthhvM6EDvbQ0eYHL2BEG1awh19dCdI11I1+3rNd5BjHXiGlZrw3XJOGgNfuz'
+bot_id = "U5AN3Y04SD"
+bot_alias_id = "TSTALIASID"
