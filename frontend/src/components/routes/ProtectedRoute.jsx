286b4373dad680c66eefe840c524157f300f65a6
Abhishek Uppe <ab577089@dal.ca>
2022-07-24 19:13:05 -0300

    updated login

--- frontend/src/components/routes/ProtectedRoute.jsx
+++ frontend/src/components/routes/ProtectedRoute.jsx
@@ -1,8 +1,29 @@
-import {Route} from "react-router-dom";
+import {Redirect, Route} from "react-router-dom";
+import { routes } from "../../constants";
 
 function ProtectedRoute({component: Component, ...rest}) {
 
     // add authentication
+
+    const unAuthRoutes = ["register", "login", "forgotpassword", "changepassword", "question-Answer", "ceaser-cipher"];
+    const pathName = window.location.pathname.split("/")[1];
+
+    if (localStorage.getItem("CurrentUser")) {
+        if (unAuthRoutes.includes(pathName)) {
+            return (
+                <Redirect to={routes.roomBooking} />
+            )
+        }
+    }
+
+    if (!localStorage.getItem("CurrentUser")) {
+        if (!unAuthRoutes.includes(pathName)) {
+            return (
+                <Redirect to={routes.login} />
+            )
+        }
+    }
+    
 
     return (
         <Route {...rest}
