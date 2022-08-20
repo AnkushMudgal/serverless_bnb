966624394a1415ed2b433403f9bdd76d66bc8965
unknown <dm341240@dal.ca>
2022-08-20 00:52:00 -0300

    conf files updated

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDmLlebcOZmibYEUBhYpwZdXwag6j3ofvA",
-    authDomain: "robotic-insight-340814.firebaseapp.com",
-    projectId: "robotic-insight-340814",
-    storageBucket: "robotic-insight-340814.appspot.com",
-    messagingSenderId: "660050408812",
-    appId: "1:660050408812:web:d6d5578cd16048cb71af49",
-    measurementId: "G-K8MXF55MNK"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const app = initializeApp(firebaseConfig);
